5849ecaefc620da291a23662a3af72ec4ed43db1
sh-v <124066783+sh-vp@users.noreply.github.com>
2024-09-28 01:25:33 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,24 +13,46 @@ read -p "Please Insert Maximum CPU Threshold :" cpu
 echo -e  ""
 read -p "Please Insert Maximum Memmory Threshold :" ram
 echo -e  ""
+read -p "Please ENter Send Alert Method (1-Telegram_bot | 2-Gotify :" mod
+echo -e  ""
+if [[ ${mod} == 2 ]]; then
+read -p "Please Insert Your Gotify Login Domain or IP :" id
+echo -e  ""
+read -p "Please Insert Your Gotify API_TOKEN:" token
+echo -e  ""
+else
 read -p "Please Insert Your Telegram CHAT_ID :" id
 echo -e  ""
 read -p "Please Insert Your Telegram BOT_TOKEN:" token
 echo -e  ""
+fi
 echo -e  ""
 echo -e  "${yellow}Start installing Script ...${White}"
 
 wget --no-check-certificate -O /var/log/monitor.sh https://raw.githubusercontent.com/sh-vp/server-monitoring/main/monitoring.sh
 chmod +x /var/log/monitor.sh
-croncmd1="bash /var/log/monitor.sh -c ${cpu} -r ${ram} -i ${id} -t ${token} > /dev/null 2>&1"
-croncmd2="sleep 30 && bash /var/log/monitor.sh -i ${id} -t ${token} > /dev/null 2>&1"
-cronjob1="*/5 * * * * $croncmd1"
-cronjob2="@reboot $croncmd2"
-( crontab -l | grep -v -F "$croncmd1" ; echo "$cronjob1" ) | crontab -
-( crontab -l | grep -v -F "$croncmd2" ; echo "$cronjob2" ) | crontab -
 SERVER_IP=$(hostname -I | awk '{print $1}')
 SERVER_HOSTNAME=$(hostname)
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+
+if [[ ${mod} == 2 ]]; then
+croncmd1="bash /var/log/monitor.sh -m 2 -c ${cpu} -r ${ram} -i ${id} -t ${token} > /dev/null 2>&1"
+croncmd2="sleep 30 && bash /var/log/monitor.sh -m 2 -i ${id} -t ${token} > /dev/null 2>&1"
+    url_msg="${token}/message?token=${id}"
+    read -r -d '' msg <<EOT
+📌 <b>Host Name: $SERVER_HOSTNAME</b>
+
+🌐 <b>IP Address : $SERVER_IP</b>
+
+📅 <b>Time : $TIMESTAMP</b>
+
+--
+EOT
+    curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" \
+        -d "{\"title\":\"✅ Server Monitor Installed Successfully !\",\"message\":\"${msg}",\"priority\":5}" \"$url_msg"
+else
+croncmd1="bash /var/log/monitor.sh -m 1 -c ${cpu} -r ${ram} -i ${id} -t ${token} > /dev/null 2>&1"
+croncmd2="sleep 30 && bash /var/log/monitor.sh -m 1 -i ${id} -t ${token} > /dev/null 2>&1"
 read -r -d '' msg <<EOT
 ✅ <b>Server Monitor Installed Successfully !</b>
 
@@ -44,5 +66,10 @@ read -r -d '' msg <<EOT
 EOT
 
 curl --data chat_id="${id}" --data-urlencode "text=${msg}" "https://api.telegram.org/bot${token}/sendMessage?parse_mode=HTML" > /dev/null
+fi
+cronjob1="*/5 * * * * $croncmd1"
+cronjob2="@reboot $croncmd2"
+( crontab -l | grep -v -F "$croncmd1" ; echo "$cronjob1" ) | crontab -
+( crontab -l | grep -v -F "$croncmd2" ; echo "$cronjob2" ) | crontab -
 clear
 echo -e  "Server Monitor Installed ${green}Successfully !${White}"
